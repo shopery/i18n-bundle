e3b7941c491e05700df7401ac384ff3781e787cb
Berny Cantos <be@rny.cc>
2016-10-21 11:45:06 +0200

    add bundle

---
+++ I18nBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Shopery\Bundle\I18nBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class I18nBundle extends Bundle
+{
+    protected function createContainerExtension()
+    {
+        return null;
+    }
+}
